c657873de94225161b9e1f4abd3af3e93d37b27e
Aldiansyah Dwi P <aldiansyhdp@gmail.com>
2023-06-07 09:11:38 +0700

    add header and input

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import { useState } from 'react';
-import './App.css';
 
 function App() {
-  return <></>;
+  const [searchInput, setSearchInput] = useState('');
+
+  return (
+    <main className='max-w-screen-lg mx-auto font-inter py-5'>
+      <header className='flow flex flex-col items-center'>
+        <h1 className='text-[2.5rem] font-bold'>Photo Club Gallery</h1>
+        <input
+          type='text'
+          className='bg-gray-700 block text-white-900 px-3 p-2 text-lg rounded-md w-[32ch] focus:outline-none placeholder:text-gray-400 focus:ring-2 focus:ring-blue-500'
+          id='search-filter'
+          value={searchInput}
+          placeholder='Search'
+          onChange={e => setSearchInput(e.target.value)}
+        />
+      </header>
+    </main>
+  );
 }
 
 export default App;
